3f23d94d7eb4616f7796d1b564c8e4f24efebc2f
Michael Hladky <michael@hladky.at>
2023-01-11 03:15:47 +0100

    fix: adopt typing

--- packages/node-cli-testing/cli-project/src/lib/factory.ts
+++ packages/node-cli-testing/cli-project/src/lib/factory.ts
@@ -2,7 +2,7 @@ import {CliProject} from "./cli";
 import {ProjectConfig} from "./types";
 
 export class CliProjectFactory {
-  static async create<T>(cfg: ProjectConfig<T>): Promise<CliProject<T>> {
+  static async create<T extends {}>(cfg: ProjectConfig<T>): Promise<CliProject<T>> {
     const prj = new CliProject<T>();
     await prj._setup(cfg);
     return prj;
